2431351cd7df6bf83ae9f77d072ff85f2612f48b
william1616 <william1616@users.noreply.github.com>
2013-09-24 20:45:23 +0000

    fixed errors

--- GPIOlib.py
+++ GPIOlib.py
@@ -3,22 +3,22 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-def setOut(pin)
+def setOut(pin):
   GPIO.setup(pin, GPIO.OUT)
 
-def setIn(pin)
+def setIn(pin):
   GPIO.setup(pin, GPIO.IN)
   
-def toggle(pin)
+def toggle(pin):
   try:
     GPIO.output(pin, not GPIO.input(12))
-  else: # change to except at later date to only catch setup errors
+  except: # change to except at later date to only catch setup errors
     setOut(pin)
     GPIO.output(pin, not GPIO.input(12))
 	
-def out(pin, state)
+def out(pin, state):
   try:
     GPIO.output(pin, state)
-  else: # change to except at later date to only catch setup errors
+  except: # change to except <error> at later date to only catch setup errors
     setOut(pin)
     GPIO.output(pin, state)
--- listner.py
+++ listner.py
@@ -4,13 +4,14 @@ import time
 import sys
 import GPIOlib as GPIO
 
-PORT = 42001
-HOST = 'localhost'
-
-print("Connecting...")
-scratchSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a new local socket of type SOCK_STREAM
-scratchSock.connect((HOST, PORT)) # connect the socket to scratch 
-print("Connected!")
+if __name__ == '__main__':
+	PORT = 42001
+	HOST = 'localhost'
+	
+	print("Connecting...")
+	scratchSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a new local socket of type SOCK_STREAM
+	scratchSock.connect((HOST, PORT)) # connect the socket to scratch 
+	print("Connected!")
 
 def sendScratchCommand(cmd):
 	# split the command into its componant bits
@@ -22,28 +23,36 @@ def sendScratchCommand(cmd):
 	a.append(chr(n & 0xFF))
 	scratchSock.send(a.tostring() + cmd)
 	
-def run_command(cmd)
-	if cmd[:3] == 'pin':
-		pin = state = ''
-		for i in cmd [3:5]:
-			#if isinstance(i, int):
-			try:
-				int(i)
-				pin += i
-			except ValueError:
-				break
-		pin = int(pin)
-		for i in cmd[3:]:
-			try:
-				str(i)
-				state += i
-			except ValueError:
-				pass
-		if state == 'on':
-			state = 1
-		elif state == 'off':
-			state = 0
-		GPIO.out(pin, state)
+def run_command(cmd):
+	if cmd[:9] == 'broadcast':
+		cmd = cmd[11:len(cmd)-1] # quotation marks and spaces
+		if cmd[:3] == 'pin':
+			pin = state = ''
+			for i in cmd[3:5]:
+				#if isinstance(i, int):
+				try:
+					int(i)
+					pin += i
+				except ValueError:
+					break
+			pin = int(pin)
+			for i in cmd[3:]: # possibly change to 16 as 14th character should always be a pin number
+				try:
+					int(i)
+				except ValueError:
+					state += i
+			if state == 'on':
+				state = 1
+			elif state == 'off':
+				state = 0
+			print 'pin: '+str(pin)+' state: '+str(state)
+			if isinstance(state, int):
+				GPIO.out(pin, state)
+		else:
+			print 'not'
 
-while True:
-	run_command(scratchSock.recv(4096))
+if __name__ == '__main__':
+	while True:
+		m = scratchSock.recv(4096)
+		# the first four characters are blank so remove them before processing broadcast command
+		run_command(m[4:])
