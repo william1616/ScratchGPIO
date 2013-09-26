a17053e2808b506bcf2b8c64833fd7b73b305d7c
william1616 <william1602@fake.com>
2013-09-26 18:19:27 +0100

    finished pwm initial
    still contains test code

--- GPIOlib.py
+++ GPIOlib.py
@@ -26,18 +26,30 @@ def out(pin, state):
 
 def cleanup():
   GPIO.cleanup()
-  print("GPIO Clean Up Done")
 
-def updatepwm(pin [,freq, dc, state]):
-  if pin in pwms[:][1]:
-    pwm = pwms[pin]
-    if pwd[4] == True:
-      pwm[0].start(pwm[3])
-    
-    
+def updatepwm(pin, freq, dc, state):
+  #pwms[][0, 1, 2, 3, 4]
+  #pwms[][<pin>, <freq>, <dc>, <state>, <object>]
+  change = [pin, freq, dc, state]
+  for i in pwms[:]:
+    if i[0] == pin:
+      for j in len(change[:]):
+        #if i[j-1] != change[j-1]:
+        i[j-1] = change[j-1]
+      i[4].ChangeFrequency(i[1])
+      i[4].ChangeDutyCycle(i[2])
+      if i[3] == True:
+        i[4].start(i[2])
+      elif i[3] == False:
+        i[4].stop()      
 
 def new_pwm(pin, freq, dc, state):
-  pwms.append(['pwm' + pin, pin, freq, dc, state])
-  if pin in pwms[1]:
-    pwms[0] = GPIO.PWM(pin, freq)
+    for i in pwms[:]:
+      if i[0] == pin:
+        break
+    pwms.append([pin, freq, dc, state, GPIO.PWM(pin, freq)])
+    #pwms[len(pwms[:])-1][0]
+    updatepwm(pin)
   
+if __name__ == '__main__':
+  new_pwm(7, 50, 50, True)
