49398dab64a9f47bea068525f1b4ded9cac2d81b
william1616 <william1616@users.noreply.github.com>
2014-06-17 16:27:10 +0100

    Refactor Listner.py
    untested

--- listner.py
+++ listner.py
@@ -1,86 +1,73 @@
-from array import array
-import socket
-import time
-import sys
+import logging, logging.handlers, socket
 import GPIOlib as GPIO
 
 if __name__ == '__main__':
-    PORT = 42001
-    print 'Please enter host IP (if using a local instance of Scratch \'localhost\')'
-    HOST = raw_input()
-    if not HOST:
-        print 'No host specified\nExiting'
-        sys.exit()
-    print "Connecting..."
+    logHost = 'localhost'
+    logPort = 'localhost'
+    scratchHost = input('Please enter host IP (if using a local instance of Scratch \'localhost\'): ')
+    scratchPort = 42001
+    
+    log = logging.getLogger('listner')
+    log.addHandler(logging.FileHandler('listner.log'))
+    #log.addHandler(logging.handlers.SocketHandler(logHost, logPort))
+    
+    if not scratchHost:
+        log.critical('No host specified')
+        print('No host specified')
+        raise
     scratchSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a new local socket of type SOCK_STREAM
+    print('Connecting...')
+    log.info('Connecting...')
     try: 
-        scratchSock.connect((HOST, PORT)) # connect the socket to scratch 
-    except: #add error type here
-        print 'Have you enabled remote connections in scratch and do you have a scratch instance running?'
-        sys.exit()
-    print "Connected!" 
-
-def sendScratchCommand(cmd):
-    # split the command into its componant bits
-    n = len(cmd)
-    a = array('c')
-    a.append(chr((n >> 24) & 0xFF))
-    a.append(chr((n >> 16) & 0xFF))
-    a.append(chr((n >>  8) & 0xFF))
-    a.append(chr(n & 0xFF))
-    scratchSock.send(a.tostring() + cmd)
-        
-def broadcast(cmd):
-    if cmd[:3] == 'pin':
-        pin = state = ''
-        for i in cmd[3:5]:
-            try:
-                int(i)
-                pin += i
-            except ValueError:
-                break
-        pin = int(pin)
-        for i in cmd[3:]:
-            try:
-                int(i)
-            except ValueError:
-                state += i
-        if state == 'on':
-            state = 1
-        elif state == 'off':
-            state = 0
-        if isinstance(state, int):
-            GPIO.out(pin, state)
-            print 'pin: '+str(pin)+' state: '+str(state)
-        else:
-            print 'Unknown state: '+state
+        scratchSock.connect((scratchHost, scratchPort)) # connect the socket to scratch 
+    except ConnectionRefusedError:
+        print('Have you enabled remote connections in scratch and do you have a scratch instance running?')
+        log.critical('Could Not Connect to Socket Address: %s Port: %d', scratchHost, scratchPort)
+        raise
+    print("Connected!")
+    log.info("Connected!")
+    
+def broadcast(**pin):
+  try:
+    if pin['state']:
+        GPIO.out(pin, state)
     else:
-        print 'Broadcast command not recognised\nTry pin<pin-number><on/off>/\e.g pin7on'
+        GPIO.toggle(pin)
+  except:
+    log.warning('Invalid Argument Supplied - Pin: %s State: %s', pin['number'], pin['state'])
+    
+def pulseWidthMod(**pin):
+    pass
 
-def cmdtype(msg):
-    i = 0
-    commands = []
-    types = ['sensor-update', 'broadcast']
-    while i <= len(msg):
-        for j in types:
-            if msg[i:i + len(j)] == j:
-                commands.append([j, i])
-        i += 1
-    for i in commands:
-        j = i[1] + len(i[0]) + 2
-        while j <= len(msg):
-            if msg[j] == '\"':
+class cmdType():
+        def __init__(self, name, argCnt, func):
+          self.name = name
+          self.argCnt = argCnt
+          self.func = func
+
+def spliceCmd(commandString):
+  types = [
+          cmdType('broadcast', 2, lambda args: broadcast(number=args[0], state=args[1])),
+          ]
+  commandList = commandString.split()
+  i = 0
+  while i < len(commandList):
+    for type in types:
+          if type.name == commandList[i]:
+            args = []
+            while len(args) < type.argCnt:
+              i += 1
+              if commandList[i] == '&&':
+                while len(args) < type.argCnt:
+                    args.append(None)
                 break
-            j += 1
-        i.append(msg[i[1] + len(i[0]) + 2:j])
-    for i in commands:
-        print i
-        if i[0] == 'broadcast':
-            broadcast(i[2])
+              args.append(commandList[i])
+            type.func(args)
+    i += 1
 
 if __name__ == '__main__':
     print 'Scratch Listner running.\nBroadcast commands in the form:\n\tpin<pin-number><on/off>\n\te.g pin7on'
     while True:
         m = scratchSock.recv(4096)
         print 'message ('+m+') received'
-        cmdtype(m)
+        spliceCmd(m)
